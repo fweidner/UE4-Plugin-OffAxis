96ed78f0b620c18344c23f06d522539ab3c42ff0
Florian Weidner <florian.weidner@tu-ilmenau.de>
2018-10-22 10:31:16 +0200

    Added local player file

---
+++ Source/OffAxisProjection/Public/OffAxisLocalPlayer.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/LocalPlayer.h"
+#include "OffAxisLocalPlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class OFFAXISPROJECTION_API UOffAxisLocalPlayer : public ULocalPlayer
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
